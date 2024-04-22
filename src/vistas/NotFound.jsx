25b606ba7851903739deed88786a80ca6d1cc35d
fcolabbe <flabbe@eldia.la>
2024-04-21 22:00:25 -0400

    Correccion estilos para moviles

--- src/vistas/NotFound.jsx
+++ src/vistas/NotFound.jsx
@@ -6,9 +6,8 @@ const NotFound = () => {
   return (
     <div className='contenedor-principal'>
       <h1>Lo siento...</h1>
-      <img src="public/404.png" alt="Not Found" />
+      <img id="notfound" src="public/404.png" alt="Not Found" />
       <h2>Pokemon no encontrado</h2>
-{/*       <button className="btn btn-dark" onClick={() => window.history.back()}>Volver</button> */}
       <NavLink to="/" className="btn btn-dark">Volver</NavLink>
     </div>
   )
